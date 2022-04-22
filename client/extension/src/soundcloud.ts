e7e2b00e1dba0eb1d1e4684c1f3281a0821332f2
Carbide Codes <carbidecodes@gmail.com>
2022-04-22 09:45:31 -0500

    Get soundcloud current song

--- client/extension/src/soundcloud.ts
+++ client/extension/src/soundcloud.ts
@@ -1,1 +1,44 @@
+import { init } from '/channel/connection.ts'
+import { Read } from 'utils/dom.ts'
+import { tap, compose } from 'common/utils/func.ts'
+
+const { select, observe, query, text } = Read
+
+const connection = init()
+connection.onOpen(() => console.log('connected.'))
+connection.onMessage(tap)
+
 document.body.style.backgroundColor = 'red'
+
+const playback = select('div.playbackSoundBadge')
+
+const extract = (node: Element) => {
+    let q = query(node)
+
+    return {
+        artist: text(q<HTMLAnchorElement>('a.sc-link-secondary')!),
+        title: text(q<HTMLSpanElement>('a.sc-link-primary span[aria-hidden=true]')!),
+        imgUrl: q<HTMLSpanElement>('div.image span')!.style.backgroundImage
+    }
+}
+
+const tapA = (x: any, label = '') => tap(x, 'asdf - ' + label)
+
+const handleUpdate = (record: MutationRecord) => {
+    switch (record.type) {
+        case 'childList':
+            tapA(extract(playback))
+            break;
+        default:
+            tapA({record}, 'unknown record')
+            
+    }
+}
+
+observe(
+    tapA(playback),
+    compose(tapA, handleUpdate),
+    {
+        childList: true
+    }
+)
--- client/extension/start_chrome.ps1
+++ client/extension/start_chrome.ps1
@@ -1,2 +1,2 @@
-start "chrome.exe" "--user-data-dir=$($HOME)/ChromeTest/ --load-extension=$($PSScriptRoot)\manifest.json --no-first-run"
+start "chrome.exe" "--user-data-dir=$($HOME)/ChromeTest/ --load-extension=$($PSScriptRoot) --no-first-run"
 # Need to manually load it first?
