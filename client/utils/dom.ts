98e20d136dc872909cc9b7f326ed7a91522d08f9
Carbide Codes <carbidecodes@gmail.com>
2022-04-22 09:45:12 -0500

    Dom and func utils

--- client/utils/dom.ts
+++ client/utils/dom.ts
@@ -1,27 +1,51 @@
-export const select = (selector: string) : Element => {
-    const el = document.querySelector(selector)
+export namespace Create {
+    export const el = <K extends keyof HTMLElementTagNameMap>(tag: K, className?: string) => {
+        const el = document.createElement<K>(tag)
+        if (className !== undefined) {
+            el.className = className
+        }
 
-    if (el == null) {
-        throw `Failed to select ${selector}`
+        return el
+    }
+    export const at = (node: Element, children: Array<Element>) => {
+        children.forEach(x => node.appendChild(x))
+        return node
+    }
+    export const text = (txt: string) => {
+        const t = el('span')
+        t.innerText = txt
+        return t
+    }
+}
+
+export namespace Read {
+    export const select = (selector: string) : Element => {
+        const el = document.querySelector(selector)
+
+        if (el == null) {
+            throw `Failed to select ${selector}`
+        }
+
+        return el
     }
 
-    return el
-}
+    export const observe = (
+        node: Element,
+        handle: (_: MutationRecord) => void,
+        opts = {characterData: true} as MutationObserverInit
+    ) => {
+        const observer = new MutationObserver(
+            (records, _) => {
+                records.forEach(handle)
+            })
 
-export const el = <K extends keyof HTMLElementTagNameMap>(tag: K, className?: string) => {
-    const el = document.createElement<K>(tag)
-    if (className !== undefined) {
-        el.className = className
+        observer.observe(node, opts)
     }
 
-    return el
+    export const query = (node: Element) =>
+        <E extends Element>(selector: string) : E | null =>
+                node.querySelector(selector)
+
+    export const text = (node: HTMLElement) => node.innerText
 }
-export const at = (node: Element, children: Array<Element>) => {
-    children.forEach(x => node.appendChild(x))
-    return node
-}
-export const text = (txt: string) => {
-    const t = el('span')
-    t.innerText = txt
-    return t
-}
+
--- common/utils/func.ts
+++ common/utils/func.ts
@@ -1,2 +1,8 @@
 export const konst = (x: any) => () => x
-export const tap = (x: any) => { console.log(x); return x }
+export const tap = (x: any, label?: string) => {
+    console.log(label ?? 'tap', x)
+    return x
+}
+
+export const compose = <R,T,U>(f: (_:R)=> T, g: (_:T) => U) => (x: R) => g(f(x))
+
